0b17e9ba2f5ffe67a4b06b2602e568aec528c3d5
user <na1212@bs.amalnet.k12.il>
2019-11-06 10:42:48 +0200

    l

---
+++ app/src/main/java/com/example/navemenu/FourColorList.java
@@ -0,0 +1,13 @@
+package com.example.navemenu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FourColorList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_four_color_list);
+    }
+}
